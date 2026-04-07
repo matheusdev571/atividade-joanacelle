05954fdb7f68c6e9d8d7d228ff3339c24bdb6c00
matheusdev571 <matheush_enriquee@icloud.com>
2026-04-07 09:26:23 -0300

    Update #13.py

--- atividades dia 31/#13.py
+++ atividades dia 31/#13.py
@@ -1,4 +1,4 @@
-#13
+#12  Leia dois números e: Mostre a soma; Mostre qual é maior ou se são iguais
 a= float(input("digite um numero: "))
 b= float(input("digite um numrero: "))
       print("soma:", (a + b))

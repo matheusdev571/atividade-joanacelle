128b40e0c577a18bbe6cb7dc6dbb5e3c850a909b
matheusdev571 <matheush_enriquee@icloud.com>
2026-04-07 09:24:27 -0300

    Update #12.py

--- atividades dia 31/#12.py
+++ atividades dia 31/#12.py
@@ -1,11 +1,10 @@
-#12
+#11 Leia um número e: Se for par e positivo → “Par positivo”; Se for par e negativo → “Par
+negativo”; Caso contrário → “Ímpar”
 a= int(input("digite um numero: "))
 if ( a % 2 ==0 ):
   if (a > 0 ):
   print("par positivo")
- elif (a > 0:
+else:
   print("par negativo")
- else:
-  print("par (zero) ")
- else:
+else:
   print("impar")

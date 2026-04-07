810049892a0d7b08c2d44972d6f29e5284ec7705
matheusdev571 <matheush_enriquee@icloud.com>
2026-04-07 09:21:15 -0300

    Update #10.py

--- atividades dia 31/#10.py
+++ atividades dia 31/#10.py
@@ -1,4 +1,5 @@
-#10
+#10 Leia um número e informe: “Dentro do intervalo” se estiver entre 0 e 10; “Fora do intervalo”
+caso contrário.
 a= int(input("digite um número: "))
 if (a >= 0):
  if (a <= 10):

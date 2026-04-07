30754aab91b17d8a8913174c431ec05f42c620fe
matheusdev571 <matheush_enriquee@icloud.com>
2026-04-07 09:19:36 -0300

    Update #8.py

--- atividades dia 31/#8.py
+++ atividades dia 31/#8.py
@@ -1,4 +1,5 @@
-#8
+#8  Leia um número e: Se for positivo, mostre a raiz aproximada (use **0.5); Caso contrário,
+informe “Número inválido”
  a = int(imput("digite um numero: "))
 if (a >=0) :
   print ("raiz aproximada:" , a ** 0.5)
